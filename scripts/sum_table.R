09a1a097811c6f573a8de13e224630a07fa9f1e5
Vesso927 <54169475+Vesso927@users.noreply.github.com>
2019-11-18 10:12:13 -0800

    wrapping

--- scripts/sum_table.R
+++ scripts/sum_table.R
@@ -1,21 +1,50 @@
 # file for summary table
 
-source("../scripts/cost_of_living.r")
-library(dplyr)
+get_sum_table <- function(df_temp, df_temp2, df_temp3) {
+  df_city_cost <- df_temp %>%
+    filter(nchar(Country) == 2) %>%
+    select(City, Country, Cost.of.Living.Plus.Rent.Index) %>%
+    arrange(Country)
+  
+  df_city_pop <- df_temp2 %>%
+    filter(grepl("2016", Year) & Measures == "Total Homeless") %>%
+    select(State, CoC.Name, Count)
+  
+  df_city_pop$CoC.Name <- gsub("/", " ", df_city_pop$CoC.Name)
+  
+  df_city_pop$City <- word(df_city_pop$CoC.Name, 1)
+  a <- left_join(df_city_cost, df_city_pop, by = "City")
+  
+  df_city_pop$City <- word(df_city_pop$CoC.Name, 1, 2)
+  b <- left_join(df_city_cost, df_city_pop, by = "City")
+  
+  df_combined <- rbind(a, b, by = "City") %>%
+    na.omit() %>%
+    filter(Country == State) %>%
+    slice(-94) %>%
+    select(City, State, Cost.of.Living.Plus.Rent.Index, Count) %>%
+    rename(CPI = Cost.of.Living.Plus.Rent.Index) %>%
+    mutate(Count = gsub(",", "", Count)) %>%
+    mutate(Count = as.numeric(Count)) %>%
+    mutate(CPI = as.numeric(CPI)) %>%
+    group_by(State) %>%
+    mutate(avg.cpi = mean(CPI)) %>%
+    mutate(total_hl = sum(Count))
 
-pop_by_state <- df_combined %>%
-  group_by(State) %>%
-  mutate(avg.cpi = mean(CPI)) %>%
-  mutate(total_hl = sum(Count))
+  df_temp3$GEO.display.label <- state.abb[match(df_temp3$GEO.display.label, state.name)]
+  df_temp3$respop72016 <- as.numeric(df_temp3$respop72016) 
 
-df_temp <- read.csv("../data/homelessness/Population-by-state.csv", stringsAsFactors = FALSE)
-df_temp$GEO.display.label <- state.abb[match(df_temp$GEO.display.label, state.name)]
-df_temp$respop72016 <- as.numeric(df_temp$respop72016) 
-
-sum_table <- pop_by_state %>%
-  left_join(df_temp, by = c("State" = "GEO.display.label")) %>%
-  mutate(percentage = 100*total_hl/respop72016) %>%
-  select(State, avg.cpi, total_hl, respop72016, percentage) %>%
-  distinct() %>%
-  arrange(desc(percentage)) %>%
-  head(n = 10)
+  sum_table <- df_combined %>%
+    left_join(df_temp3, by = c("State" = "GEO.display.label")) %>%
+    mutate(percentage = 100*total_hl/respop72016) %>%
+    select(State, avg.cpi, total_hl, respop72016, percentage) %>%
+    distinct() %>%
+    arrange(desc(percentage)) %>%
+    head(n = 10)
+  
+  sum_table <- kable(sum_table, digits = 2, align = "l",
+                     col.names = c("State", "Avg.CPI", "Homeless Population",
+                                   "Total Population", "Percentage(%)"),
+                     caption = "Summary Table")
+  return(sum_table)
+}
