799126a62ec8a3718cc391ea14c332cc32de0cc4
gfong2017 <gfong1999@gmail.com>
2019-11-18 18:19:32 -0800

    lint error corrections

--- scripts/crime_rate.R
+++ scripts/crime_rate.R
@@ -4,32 +4,37 @@ library(dplyr)
 library(plotly)
 
 # Function to make the chart data_one = homelessness // data_two = crime
-get_scatterplot <- function(data_one, data_two){
-  
+get_scatterplot <- function(data_one, data_two) {
   twenty_sixteen_homepop <- data_one %>%
-    mutate(year_num = (as.Date(data_one$Year, format="%d/%m/%Y")),
-           Count = as.numeric(as.factor(Count))) %>%
-    filter(year_num == "2016-01-01", Measures == "Total Homeless") %>% 
+    mutate(
+      year_num = (as.Date(data_one$Year, format = "%d/%m/%Y")),
+      Count = as.numeric(as.factor(Count))
+    ) %>%
+    filter(year_num == "2016-01-01", Measures == "Total Homeless") %>%
     group_by(State) %>%
     summarize(state_hl = sum(Count)) %>%
     filter(State != "GU", State != "PR", State != "VI") %>%
-    mutate(homeless_per_100000 = (state_hl)/100000)
-  
+    mutate(homeless_per_100000 = (state_hl) / 100000)
+
   crime_by_state <- data_two %>%
-    mutate(state = substr(data_two$county_name,
-                          nchar(data_two$county_name)-2,
-                          nchar(data_two$county_name))) %>%
+    mutate(state = substr(
+      data_two$county_name,
+      nchar(data_two$county_name) - 2,
+      nchar(data_two$county_name)
+    )) %>%
     group_by(state) %>%
     select(state, crime_rate_per_100000) %>%
     summarize(crime_rate_per_100000 = mean(crime_rate_per_100000))
-  
-  crime_hl_by_state<- data.frame(twenty_sixteen_homepop$State, 
-                                 twenty_sixteen_homepop$homeless_per_100000,
-                                 crime_by_state$crime_rate_per_100000)
+
+  crime_hl_by_state <- data.frame(
+    twenty_sixteen_homepop$State,
+    twenty_sixteen_homepop$homeless_per_100000,
+    crime_by_state$crime_rate_per_100000
+  )
   homeless_v_crime <- plot_ly(
     data = crime_hl_by_state,
-    x = ~twenty_sixteen_homepop.homeless_per_100000, 
-    y = ~crime_by_state.crime_rate_per_100000, 
+    x = ~twenty_sixteen_homepop.homeless_per_100000,
+    y = ~crime_by_state.crime_rate_per_100000,
     type = "scatter",
     alpha = .7,
     hovertext = crime_hl_by_state$twenty_sixteen_homepop.State,
@@ -37,10 +42,10 @@ get_scatterplot <- function(data_one, data_two){
   ) %>%
     layout(
       title = "Homelessness vs Crime",
-      yaxis= list(title = "State Crime Rates Per 100000"),
-      xaxis= list(title = "State Homelessness Rates Per 100000")
+      yaxis = list(title = "State Crime Rates Per 100000"),
+      xaxis = list(title = "State Homelessness Rates Per 100000")
     )
-  
- 
+
+
   return(homeless_v_crime)
 }
