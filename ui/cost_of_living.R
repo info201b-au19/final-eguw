d10ac67284711e21670d368b3f91f19798ff5e4f
Vesso927 <54169475+Vesso927@users.noreply.github.com>
2019-12-02 05:53:20 -0800

    update cost_of_living page and scripts / linting

--- ui/cost_of_living.R
+++ ui/cost_of_living.R
@@ -8,7 +8,9 @@ cost_of_living <- tabPanel(
       selectInput(
         inputId = "control1",
         label = "Choose an Index",
-        choices = c("Cost.of.Living.Index", "Rent.Index", "Cost.of.Living.Plus.Rent.Index", "Groceries.Index", "Local.Purchasing.Power.Index", "Monthly.Pass"),
+        choices = c("Cost.of.Living.Index", "Rent.Index",
+                    "Cost.of.Living.Plus.Rent.Index", "Groceries.Index",
+                    "Local.Purchasing.Power.Index", "Monthly.Pass"),
         selected = "Cost.of.Living.Plus.Rent.Index"
       ),
       checkboxInput("control2", label = "Smoothed Curve", value = FALSE),
@@ -32,16 +34,17 @@ cost_of_living <- tabPanel(
       htmlOutput(outputId = "space"),
       verbatimTextOutput(outputId = "note"),
       p("This bar chart includes 10 representative cities ranging from cities
-        with below average CPIs(or your choice of Index) to those with the higest
-        CPIs. As you can tell, as the CPI goes up, the Homelessness rates goes up.
-        We have calculated the number of Homeless people in 1 Million people to
-        eliminate the influence by city population(larger cities tend to have
-        higher CPIs and higher population, which means more homeless people). Note
-        that Atlanta and Seattle are two outlier in this comparison, with unusual
-        high population of Homeless. However, this can't deny that Homeless rate is
-        correlated with CPI. We should be cocerned that Seattle's high homeless
-        population has something to do with other factors such as lack of
-        governance and bad drug control, etc.", style = "font-size:16pt"
+        with below average CPIs(or your choice of Index) to those with the
+        higest CPIs. As you can tell, as the CPI goes up, the Homelessness
+        rates goes up. We have calculated the number of Homeless people in 1
+        Million people to eliminate the influence by city population(larger
+        cities tend to have higher CPIs and higher population, which means more
+        homeless people). Note that Atlanta and Seattle are two outlier in this
+        comparison, with unusual high population of Homeless. However, this
+        can't deny that Homeless rate is correlated with CPI. We should be
+        cocerned that Seattle's high homeless population has something to do
+        with other factors such as lack of governance and bad drug
+        control, etc.", style = "font-size:16pt"
       )
     )
   )
