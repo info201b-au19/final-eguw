d3dfebeee55519b6baec1ac1a838e79d99ae9c4b
Eric <eric@x-tracted.com>
2019-12-03 23:19:28 -0800

    shunk mapsize

--- ui/homeless_demographics.R
+++ ui/homeless_demographics.R
@@ -2,12 +2,11 @@
 homeless_demographics <- tabPanel(
   "Homeless Demograpics",
   titlePanel(em("Homeless Population Statistics", style = "font-family: 'Trebuchet MS'")),
-  leafletOutput("map"),
-  
   fluidRow(
     column(
       8,
       offset = 2,
+      leafletOutput("map"),
       selectInput(inputId = "analysis",
                   label = "Analysis",
                   choices = c("Shelter","Family"),
