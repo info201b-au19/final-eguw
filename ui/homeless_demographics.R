99f8a0a504fe16e061c1f87e81806f38693090f3
Eric Gabrielson <egabrielson@positiveplace.org>
2019-12-04 10:22:29 -0800

    merging

--- ui/homeless_demographics.R
+++ ui/homeless_demographics.R
@@ -1,20 +1,22 @@
 # Define interactive page 1
 homeless_demographics <- tabPanel(
   "Homeless Demograpics",
-  titlePanel(em("Homeless Population Statistics", style = "font-family: 'Trebuchet MS'")),
+  titlePanel(em("Homeless Population Statistics",
+                style = "font-family: 'Trebuchet MS'")),
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId = "analysis",
                   label = "Analysis",
-                  choices = c("Shelter","Family"),
+                  choices = c("Shelter", "Family"),
                   selected = "Shelter"),
-      p("Use the control above and map to view different aspects of the homeless population, 
-        and how they vary from state to state. You can see the ratio of sheltered to unsheltered homeless, 
-        as well as those which have family and those who do not.")
+      p("Use the control above and map to view different aspects of the 
+      homeless population, and how they vary from state to state. You can see 
+      the ratio of sheltered to unsheltered homeless, as well as those which 
+      have family and those who do not.")
     ),
     mainPanel(
       leafletOutput("map")
     )
   )
-  
+
 )
