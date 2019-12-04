fc664128b76aad5a89b1b6eb857c4eb393aeaf8b
Eric <eric@x-tracted.com>
2019-12-03 23:18:25 -0800

    added legend to maps

--- scripts/shelter.R
+++ scripts/shelter.R
@@ -5,13 +5,25 @@ library(tidyr)
 
 # return map illustrating homeless population with shelter
 shelter_analysis <- function(summary, choice) {
+  colors <- c("#3FCC87","#7C998B")
+  shelter <- summary %>% select(sheltered_homeless, unsheltered_homeless)
+  family <- summary %>% select(homeless_indiv, homeless_fam)
+  
   shelter_map <- leaflet(data = summary, width = "100%", height = "400px") %>%
     addTiles() %>%
     setView(lng = -95.7129, lat = 37.0902, zoom = 2) %>% 
     addMinicharts(
       summary$longitude, summary$latitude,
-      type = "pie",
-      chartdata = select(summary, sheltered_homeless, unsheltered_homeless)
+      chartdata = shelter,
+      colorPalette = colors,
+      legend = FALSE,
+      type = "pie"
+    ) %>% 
+    addLegend(
+      "topright",
+      colors = colors,
+      opacity = .8,
+      labels = c("Sheltered Homeless", "Unsheltered Homeless")
     )
   
   family_map <- leaflet(data = summary, width = "100%", height = "400px") %>%
@@ -19,9 +31,17 @@ shelter_analysis <- function(summary, choice) {
     setView(lng = -95.7129, lat = 37.0902, zoom = 2) %>% 
     addMinicharts(
       summary$longitude, summary$latitude,
-      type = "pie",
-      chartdata = select(summary, homeless_indiv, homeless_fam)
-    ) 
+      chartdata = family,
+      colorPalette = colors,
+      legend = FALSE,
+      type = "pie"
+    ) %>% 
+    addLegend(
+      "topright",
+      colors = colors,
+      opacity = .8,
+      labels = c("Homeless Individuals", "Homeless Families")
+    )
   
   if(choice == "Shelter"){
     return(shelter_map)   
